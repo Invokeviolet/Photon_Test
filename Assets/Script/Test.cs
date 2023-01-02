702aa477f0a6291ddeabcc966dd8bcf1fa38cc28
sora <cpflskfn@gmail.com>
2023-01-02 20:38:12 +0900

    photon test

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,19 @@
+using Photon.Pun;
+using Photon.Realtime;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+public class Test : MonoBehaviourPunCallbacks
+{
+    // 서버 상태를 확인할 텍스트
+    [SerializeField] TextMeshProUGUI StateText; 
+    // 상호작용할 버튼
+
+
+}
